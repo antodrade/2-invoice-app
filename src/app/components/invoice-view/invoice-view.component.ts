ac7b5cebfc3b5d274c877ff7d165585a4b618df4
prueba2@gmail.com <prueba@gmail.com>
2024-09-13 19:30:02 -0500

    Creando Sub-Componentes de factura

---
+++ src/app/components/invoice-view/invoice-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-invoice-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './invoice-view.component.html',
+
+})
+export class InvoiceViewComponent {
+
+}
